415b6c385547b8921835dc1b1830e2acad35c7de
Pencroff <pencroff@gmail.com>
2015-08-25 02:38:30 +0100

    Add structure of uow

---
+++ index.js
@@ -0,0 +1,15 @@
+/**
+ * Created by Pencroff on 25-Aug-15.
+ */
+
+export default class UnitOfWork {
+    constructor(config) {
+
+    }
+    registerRepo(name, config) {
+
+    }
+    getRepo(name) {
+
+    }
+}
